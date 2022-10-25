d9150ec29454a6528319e03d868215a4ed344939
antudas22 <20antu20@gmail.com>
2022-10-25 17:08:40 +0600

    Project basic setup and router setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjVxJbV9byXOOJYjgXuYwE09aQu6sxayc",
+  authDomain: "fir-learning-34e48.firebaseapp.com",
+  projectId: "fir-learning-34e48",
+  storageBucket: "fir-learning-34e48.appspot.com",
+  messagingSenderId: "370213794075",
+  appId: "1:370213794075:web:6dfee8849cc4c087d723be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
