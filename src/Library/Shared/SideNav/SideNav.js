66ad43fc3c7dc4a1a2779d65970257ea4d2b8092
antudas22 <20antu20@gmail.com>
2022-10-25 20:34:43 +0600

    Sidebar section completed

--- src/Library/Shared/SideNav/SideNav.js
+++ src/Library/Shared/SideNav/SideNav.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const SideNav = () => {
-    return (
-        <div>
-            <h2>This is side nav</h2>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/course-categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>All Category: {categories.length}</h2>
+      <div>
+        {
+            categories.map(category => <p key={category.id}>
+                <Link to={`/category/${category.id}`}><Button variant="outline-primary">{category.name}</Button></Link>
+            </p>)
+        }
+      </div>
+    </div>
+  );
 };
 
 export default SideNav;
